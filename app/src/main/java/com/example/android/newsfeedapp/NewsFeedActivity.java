d30f6ec36b43a3b258bff67d78a8d3b2d32b7184
Eugene Skinner <eugene.skinner@homes.com>
2017-01-18 23:51:06 -0500

    added xml files for settings activity

--- app/src/main/java/com/example/android/newsfeedapp/NewsFeedActivity.java
+++ app/src/main/java/com/example/android/newsfeedapp/NewsFeedActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.newsfeedapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -27,6 +30,23 @@ public class NewsFeedActivity extends AppCompatActivity implements
 
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feed);
@@ -38,6 +58,8 @@ public class NewsFeedActivity extends AppCompatActivity implements
         //TODO Check For Connection
         //TODO Build out the other methods
     }
+
+
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
